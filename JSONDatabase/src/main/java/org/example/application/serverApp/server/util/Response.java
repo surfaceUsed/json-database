d9f021e285494890a61782bd4a830f34890199a8
surfaceUsed <153752810+surfaceUsed@users.noreply.github.com>
2024-05-28 08:15:01 +0200

    Update Response.java

--- JSONDatabase/src/main/java/org/example/application/serverApp/server/util/Response.java
+++ JSONDatabase/src/main/java/org/example/application/serverApp/server/util/Response.java
@@ -11,27 +11,15 @@ public final class Response {
 
     private Response() {}
 
-    public String getResponse() {
-        return response;
-    }
-
-    public void setResponse(String response) {
+    private void setResponse(String response) {
         this.response = response;
     }
 
-    public String getReason() {
-        return reason;
-    }
-
-    public void setReason(String reason) {
+    private void setReason(String reason) {
         this.reason = reason;
     }
 
-    public JsonElement getValue() {
-        return value;
-    }
-
-    public void setValue(JsonElement value) {
+    private void setValue(JsonElement value) {
         this.value = value;
     }
 
